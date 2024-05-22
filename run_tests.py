313521615680131b3a0d985fdf3cb5cd44f1fd6c
Adam H <adamh181818@gmail.com>
2024-05-22 13:22:42 -0400

    Add run scripts

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,10 @@
 import subprocess
 import os
 
-config_file_loc = "/home/adamh/rPPG/rPPG-Toolbox/configs/my_configs"
-configs = ["PURE_PURE_UBFC-rPPG_DEEPPHYS_BASIC.yaml", "PURE_PURE_UBFC-rPPG_PHYSFORMER.yaml"]
-results_loc = "/home/adamh/rPPG/rPPG-Toolbox/results"
+config_file_loc = "/home/adamh/rPPG-Toolbox/configs/my_configs"
+#configs = ["DEEPPHYS_test_M1_mov.yaml", "DEEPPHYS_test_M1_no_mov.yaml"]
+configs = ["DEEPPHYS_train_M2.yaml"]
+results_loc = "/home/adamh/rPPG-Toolbox/results"
 
 os.makedirs(results_loc, exist_ok=True)
 
@@ -12,7 +13,7 @@ for config in configs:
     res_file = os.path.join(results_loc, config.split('.')[0] + ".txt")
     with open(res_file, 'w') as results_file:
         try:
-            subprocess.run(["bash", "-i", "/home/adamh/rPPG/rPPG-Toolbox/run_tests.sh", f"{os.path.join(config_file_loc, config)}"], stdout=results_file, stderr=subprocess.STDOUT, check=True)
+            subprocess.run(["bash", "-i", "/home/adamh/rPPG-Toolbox/run_tests.sh", f"{os.path.join(config_file_loc, config)}"], stdout=results_file, stderr=subprocess.STDOUT, check=True)
         except Exception as e:
             print(f"Config {config} errored with error {e}")
         results_file.flush()
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd /home/adamh/rPPG/rPPG-Toolbox
+cd /home/adamh/rPPG-Toolbox
 conda activate rppg-toolbox
 python main.py --config_file $1
